fb8ececeecfe2bde9539c08ea7c75f552201718d
melle bulle <melvin@salenius.se>
2026-03-25 23:48:24 +0100

    Add initial README and game structure

---
+++ game.py
@@ -0,0 +1,12 @@
+import time
+
+rooms = {
+    "living_room": {
+        "description": "You are in the living room, there is a large TV and a big couch.",
+        "exits": {"north": "kitchen", "east": "bedroom"}
+    },
+    "kitchen":{
+        "description": "",
+        "exits":{"south":"hall"}
+    }
+    }
