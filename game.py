69d5c3c3a9d640a098652dcb8d6e2900c923dc8e
melleboiii69 <melvin@salenius.se>
2026-03-27 11:48:57 +0100

    Added actions and what happens during actions

--- game.py
+++ game.py
@@ -22,5 +22,15 @@ while pet_stats["hunger"] > 0:
         
         action = input("What do you want to do?").lower()
         
-        
-        
+        if action == "feed":
+            pet_stats["hunger"] += 2
+            pet_stats["energy"] -= 1
+            print("You fed you pet +1 hunger, I am getting tired")
+        elif action == "play":
+            pet_stats["happiness"] += 2
+            pet_stats["energy"] -= 1
+        elif action == "rest":
+            pet_stats["energy"] += 3
+    
+    if pet_stats["hunger"] > 10:
+        pet_stats["hunger"] = 10
