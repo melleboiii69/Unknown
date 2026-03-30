c9fe2829dd2c4e57dca56fac9f6ec583ae2c368a
melleboiii69 <melvin@salenius.se>
2026-03-30 16:22:06 +0200

    The game works, not advanced at all and there is a bug that when input is feed or eat the hunger does not go up. Working on adding a pet "consiousnes" so it is random

--- game.py
+++ game.py
@@ -1,4 +1,14 @@
 import time
+import random
+
+
+thoughts= [
+    "I want to play!",
+    "Can we go for a walk?",
+    "I feel like napping...",
+    "Is it dinner time yet?",
+    "I'm so happy to see you!"
+]
 pet_stats = {
     "hunger": 10,
     "happiness": 10,
@@ -10,7 +20,13 @@ pet_stats = {
 while pet_stats["hunger"] > 0:
     
     pet_stats["hunger"] -= 1
+    
+    print(f"\n--- STATS: Hunger: {pet_stats['hunger']} | Happiness: {pet_stats['happiness']} | Energy: {pet_stats['energy']} ---")
 
+    current_thought = random.choice(thoughts)
+    print(current_thought)
+    action = input("What do you want to do?").lower()
+    
     if pet_stats["hunger"] <= 3:
         print("( x . x )  I am starving...")
     elif pet_stats["happiness"] <= 3:
@@ -20,17 +36,16 @@ while pet_stats["hunger"] > 0:
     else:
         print("( ^ . ^ )  I am happy!")
         
-        action = input("What do you want to do?").lower()
-        
-        if action == "feed":
+        if action in ["eat", "feed"]:
             pet_stats["hunger"] += 2
             pet_stats["energy"] -= 1
             print("You fed you pet +1 hunger, I am getting tired")
         elif action == "play":
             pet_stats["happiness"] += 2
             pet_stats["energy"] -= 1
-        elif action == "rest":
+        elif action in ["sleep", "rest"]:
             pet_stats["energy"] += 3
     
-    if pet_stats["hunger"] > 10:
-        pet_stats["hunger"] = 10
+    pet_stats["hunger"] = min(pet_stats["hunger"], 10)
+    pet_stats["energy"] = min(pet_stats["energy"], 10)
+    pet_stats["happiness"] = min(pet_stats["happiness"], 10)
